d6a5006bf17d9b960ce0a132f55961bd9eab5282
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-17 18:58:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@count += 1
+  end
+  def self.count
+    @@song.count
+  end
+
+end
